a67be784166a92d250e4688681a4a90f634ca8e8
TruongVoXuan <truong.75.cvan@gmail.com>
2024-09-13 23:18:27 +0700

    Các hàm xuất ra màn hình

---
+++ HelloWorld/src/app/HelloWorld.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//			System.out.println("Hello World");
+//	}
+	
+	//Đối tượng Scanner
+//	** Cung cấp thư viện **
+//	 import java.util.Scanner;
+//     Scanner scanner = new Scanner(System.in);
+	
+//	== Các phương thức cấp sẵn==
+//	scanner.nextLine() ==> Nhận 1 dòng từ bàn phím ==> trả ra dữ liệu string
+//	scanner.nextInt() ==> Nhận 1 số nguyên từ bàn phím ==> trả ra dữ liệu số nguyên
+//	scanner.nextDouble() ==> Nhận 1 số thực từ bàn phím ==> trả ra dữ liệu số thực
+
+//	Ví dụ:
+//	Khai báo 2 biến name và age.
+//	Nhập name và age từ bàn phím
+//	Sử dụng 3 hàm trên để in ra màn hình:
+//		My name is <name>, age= <age>
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("You name is:");
+		String name = scanner.nextLine();
+		
+		System.out.println("You age is:");
+		int age = scanner.nextInt();
+		
+		System.out.println("My name is:"+name +", age = "+ age);
+		scanner.close();
+	}
+}
--- src/truong/HelloWorld.java
+++ src/truong/HelloWorld.java
@@ -2,10 +2,7 @@ package truong;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		int a = 6;
-		int b = 9;
-		int c = a + b;
-		System.out.println("Sum:" + c);
+		int truong = 22;
 
 	}
 }
